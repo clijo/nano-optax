077de10d99399498a31700006638461cab0d38ce
clijo <52968726+clijo@users.noreply.github.com>
2026-01-26 00:40:02 +0100

    update init

--- src/nano_optax/__init__.py
+++ src/nano_optax/__init__.py
@@ -1,5 +1,5 @@
 from nano_optax.gd import GD
-from nano_optax.prox_gd import ProxGD, ProxL1
+from nano_optax.prox_gd import ProxGD, ProxL1, ProxL2
 from nano_optax.schedulers import ConstantLR, LambdaLR, LRScheduler, StepLR
 from nano_optax.sgd import SGD
 from nano_optax.types import OptResult
@@ -9,6 +9,7 @@ __all__ = [
     "SGD",
     "ProxGD",
     "ProxL1",
+    "ProxL2",
     "OptResult",
     "LRScheduler",
     "ConstantLR",
